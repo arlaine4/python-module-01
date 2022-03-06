b74b4a8fa4a22956e1ffbc6a0811b01dd3d0279f
Arthur LAINE <arlaine@student.42.fr>
2022-03-06 15:03:04 +0100

    fixed a bug on add account method, working as expected now, need to test out transfer method and do fix_account method

--- ex05/the_bank.py
+++ ex05/the_bank.py
@@ -9,6 +9,7 @@ class Account:
         self.id = self.ID_COUNT
         self.name = name
         self.__dict__.update(kwargs)
+        self.value = 0
         Account.ID_COUNT += 1
 
     def transfer(self, amount):
@@ -23,21 +24,17 @@ class Bank:
     def __init__(self):
         self.account = []
 
-    def check_new_account_validity(self, account):
-        """
-        Checking account validity.
-        - Check if account is instance of Account class
-        - Check if account.name does not exists in self.account list
-        """
-        if not isinstance(account, Account) or not self.check_and_return_account_idx(account):
-            return False
+    def check_account_not_in_accounts(self, account):
+        for acc in self.account:
+            if acc.name == account.name:
+                return False
         return True
 
     def add(self, account):
-        if not self.check_new_account_validity(account):
+        if isinstance(account, Account) and self.check_account_not_in_accounts(account):
             self.account.append(account)
         else:
-            print(f'Invalid account {account}, not adding it to the bank')
+            print(f'Invalid account {account.name}, not adding it to the bank')
 
     def check_and_return_account_idx(self, account_name):
         """
@@ -48,7 +45,7 @@ class Bank:
         for i, account in enumerate(self.account):
             if account.name == account_name:
                 return i
-        return None
+        return False
 
     def transfer(self, origin, dest, amount):
         # Checking origin parameter validity
@@ -79,7 +76,19 @@ class Bank:
         pass
 
     def __str__(self):
-        to_ret = ''
-        for account in self.account:
-            to_ret += account.__str__() + '\n\n'
+        to_ret = '\n'
+        for i, account in enumerate(self.account):
+            if i == len(self.account) - 1:
+                to_ret += account.__str__() + '\n'
+            else:
+                to_ret += account.__str__() + '\n\n'
         return to_ret
+
+
+if __name__ == "__main__":
+    bank1 = Bank()
+    account1 = Account('test')
+    bank1.add(account1)
+    account2 = Account('test')
+    bank1.add(account2)
+    print(bank1)
