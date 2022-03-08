238b5b9e33ba65d01d58e599c78bbff2809bee9d
Arthur LAINE <arlaine@e2r11p17.clusters.42paris.fr>
2022-03-08 12:02:07 +0100

    small fix and changes

--- ex01/game.py
+++ ex01/game.py
@@ -22,16 +22,15 @@ def trim_doc(docstring):
 
 
 class GotCharacter:
-    def __init__(self, house, first_name=None, is_alive=True):
+    def __init__(self, first_name=None, is_alive=True):
         self.__doc__ = trim_doc('Generic class for any GOT character')
         self.first_name = first_name
         self.is_alive = is_alive
-        self.house = house
 
 
 class Stark(GotCharacter):
     def __init__(self, first_name=None, is_alive=True):
-        super().__init__(self, first_name=first_name, is_alive=is_alive)
+        super(Stark, self).__init__(first_name=first_name, is_alive=is_alive)
         self.__doc__ = trim_doc('A class representing the Stark family. Or when bad things'
                                 'happen to good people.')
         self.family_name = 'Stark'
@@ -46,7 +45,7 @@ class Stark(GotCharacter):
 
 class Lannister(GotCharacter):
     def __init__(self, first_name=None, is_alive=True):
-        super().__init__(self, first_name=first_name, is_alive=is_alive)
+        super(Lannister, self).__init__(first_name=first_name, is_alive=is_alive)
         self.__doc__ = trim_doc('A class representing the Lannister family.')
         self.family_name = 'Lannister'
         self.house_words = 'A Lannister always pays his debts'
